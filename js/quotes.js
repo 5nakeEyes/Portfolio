3bf9b03f0e4d0034665d567e7af5d6608ec96cd0
Maciej Cieplucha <snakeeyesplay@gmail.com>
2025-08-12 00:41:00 +0200

    change files structure, add new folders, fix styles

--- js/quotes.js
+++ js/quotes.js
@@ -2,23 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const quotes = [
     {
       text: "Nawet najdłuższa podróż zaczyna się od pierwszego kroku.",
-      author: "Laozi"
+      author: "~ Laozi"
     },
     {
       text: "Nie idź tam, gdzie prowadzi droga. Idź tam, gdzie nie ma ścieżki i zostaw swój ślad.",
-      author: "Ralph Waldo Emerson"
+      author: "~ Ralph Waldo Emerson"
     },
     {
       text: "Twoje życie to twoje dzieło sztuki. Codziennie je rzeźb.",
-      author: "Bob Proctor"
+      author: "~ Bob Proctor"
     },
     {
       text: "Sekretem szczęścia jest wolność, tajemnicą wolności jest odwaga.",
-      author: "Thucydides"
+      author: "~ Thucydides"
     },
     {
       text: "Dramatem życia jest to, że starzejemy się zbyt szybko, a mądrzejemy zbyt późno.",
-      author: "Benjamin Franklin"
+      author: "~ Benjamin Franklin"
     }
   ];
 
